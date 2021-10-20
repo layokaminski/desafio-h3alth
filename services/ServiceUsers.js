e05ac9fa5bf0d5b02e16637c05b589c206b93ac1
Layo Kaminski <layokaminski@gmail.com>
2021-10-20 17:28:11 -0300

    Criado o controller de cadastro de usuarios

--- services/ServiceUsers.js
+++ services/ServiceUsers.js
@@ -1,7 +1,6 @@
 const ModelUsers = require('../models/ModelUsers');
 
 const create = async ({ firstName, lastName, birthdate, addresses }) => {
-
   const createdUser = await ModelUsers
     .create({ firstName, lastName, birthdate, addresses });
 
