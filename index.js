640cce170baf234da79dea60454e7344bc4354e3
Layo Kaminski <layokaminski@gmail.com>
2021-10-20 16:55:13 -0300

    Colocando em pratica a arquitetura MSC para desenvolver a API

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes');
 
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
   console.log(`Online na port: ${PORT}`);
